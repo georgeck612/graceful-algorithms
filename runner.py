9ba1fc66eb096524d079f0280a02a78ba974e56e
winstongergill <winstongergill612@gmail.com>
2019-12-05 14:44:43 -0600

    added test graph, sound bytes

--- runner.py
+++ runner.py
@@ -1,8 +1,11 @@
 from Graph import Graph
 from exhaustive_search import test_gracefulness
+from playsound import playsound
 
 if __name__ == '__main__':
-    graph = Graph([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
-                  [(0, 1), (1, 2), (2, 3), (2, 4), (4, 5), (4, 6), (4, 7), (7, 8), (4, 9), (9, 10), (10, 11), (10, 12),
-                   (10, 13), (13, 14)])
-    test_gracefulness(graph)
+    # test of gracefulness on K_4
+    is_graceful = test_gracefulness(Graph.generate_complete_graph(4))
+    if is_graceful:
+        playsound("res/tada.mp3")
+    else:
+        playsound("res/fail.mp3")
