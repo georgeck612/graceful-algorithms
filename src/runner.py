4894ab009785ad3c00badcff6411cd58259487a8
winstongergill <winstongergill612@gmail.com>
2019-12-22 02:56:50 -0800

    runner tests Petersen graph

--- src/runner.py
+++ src/runner.py
@@ -4,13 +4,11 @@ from src.Graph import Graph
 from src.exhaustive_search import test_gracefulness
 
 if __name__ == '__main__':
-    # test of gracefulness on K_4
-    k_5 = Graph([0,1,2,3,4,5,6],[(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(1,2),(1,3),(1,4),(1,5),(2,3),(2,4),(2,5),(3,4),(3,5),(4,5)])
-   #  k_5 = Graph.generate_complete_graph(6)
-   #  k_5.add_edge(0,6)
-    is_graceful = test_gracefulness(k_5)
-    print(k_5.graph)
-    print(k_5.order)
+    # test of gracefulness on the Petersen graph
+    petersen_graph = Graph([0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+                           [(0, 1), (0, 4), (0, 5), (1, 2), (1, 6), (2, 3), (2, 7), (3, 4), (3, 8), (4, 9), (5, 7),
+                            (5, 8), (6, 8), (6, 9), (7, 9)])
+    is_graceful = test_gracefulness(petersen_graph)
     if is_graceful:
         playsound("res/tada.mp3")
     else:
